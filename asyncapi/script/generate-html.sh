4177d92dcfa97d3d77b0b7a4d603cbeb4a0b7ea0
ayoubfargani <ayoubfargani@gmail.com>
2024-01-08 13:30:14 +0100

    update GitHub Pages

--- asyncapi/script/generate-html.sh
+++ asyncapi/script/generate-html.sh
@@ -1,1 +1,1 @@
-asyncapi generate fromTemplate ../com/ais/phoenix/asyncapi.yaml @asyncapi/html-template -o ../../docs -p singleFile=true -p outFilename='puriflash-api-doc.html'
+asyncapi generate fromTemplate ../com/ais/phoenix/asyncapi.yaml @asyncapi/html-template -o ../../docs -p singleFile=true -p outFilename='index.html'
